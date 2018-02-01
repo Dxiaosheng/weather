3ae26d4d8d176006f0fd2a78976c566e1e5cf551
sheng <1063659750@qq.com>
2018-02-01 13:31:20 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/activitytest/example/com/coolweather/db/Province.java
@@ -0,0 +1,35 @@
+package activitytest.example.com.coolweather.db;
+
+/**
+ * Created by asus on 2018/2/1.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId() {
+        return id ;
+    }
+
+    public void setId(int id) {
+        this.id = id ;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+}
